37265df328097f1c4964dfe4fb8d043b6f5313e8
Aditya-CR <54580282+Aditya-CR@users.noreply.github.com>
2021-06-15 15:31:38 +0530

    Update Program-4.c

--- Program-4.c
+++ Program-4.c
@@ -1,24 +1,23 @@
 //Program to sum odd numbers from 1 to 10
 //INPUT- 10
-//OUTPUT-25
+//OUTPUT- 25
 
 #include <stdio.h>
 int main ()
 {
  int a=10,sum = 0,i,n;
  
-//Fill the missing code
- printf("Enter n value");
 
- //Fill the missing code
+ //Read the value for n
+                                  //Correct the code : insert scanf()
+ 
 for(i=1;i<=n;i++)
  {
    
    if ( i % 2 == 0 )
       continue;
-  sum = sum + ;
+  sum = sum + ;                   //Correct the code
   }
-//Fill the missing code
 printf("%d",sum);
  return 0;
 }
