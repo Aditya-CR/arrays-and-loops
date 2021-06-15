a37b3c740f77c4714b36162109cb5ec3288e406d
Aditya-CR <54580282+Aditya-CR@users.noreply.github.com>
2021-06-15 15:27:28 +0530

    Update Program-3.c

--- Program-3.c
+++ Program-3.c
@@ -1,17 +1,16 @@
-//PRINTING THE INPUT "N"number of times
-//INPUT -3
+//Printing a Message "N" number of times
+//INPUT - 3
 //OUTPUT- I Love VVCE I Love VVCE I Love VVCE
 
 #include <stdio.h>
 int main() {
 	int i = 1, n;
-	printf("Enter an integer");
+	// Read the value for N
 	scanf("%d",&n);
- // Fill the code in scanf()
-	do {
+ 	do {
 		printf("I Love VVCE ");
 		i++;
-		// complete the statement
-	} while(i<=);
-	return(0);// complete the condition
+		
+	} while(i<=);             // Correct the code 
+	return(0);
 }
