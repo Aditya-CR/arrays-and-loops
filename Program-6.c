684f631a0c4ef6ace555d5dc7f398036e9c7a206
Aditya-CR <54580282+Aditya-CR@users.noreply.github.com>
2021-06-15 15:45:24 +0530

    Update Program-6.c

--- Program-6.c
+++ Program-6.c
@@ -1,40 +1,38 @@
-//INPUT -3 1 2 3
+//INPUT(Enter the number of elements) - 3
+//INPUT(Enter the elements) - 1 2 3
 //OUTPUT- Total number of even numbers and odd numbers in the array are 1 and 2
 
-//INSERT THE MISSING CODE 
+                           //Correct the code : include header file 
 
 int main()
 {
     int arr[10];
     int i, num, evennum, oddnum;
-
-    // Reads size and elements in array
-    printf("Enter the number of elements and the elements");
+ 
+    printf("Enter the number of elements");
     scanf("%d",&num);
-    //printf("Enter %d elements in array: ", num);
-        //printf("Enter the elements");
+    
+    printf("Enter the elements");
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);//missing code
+        scanf("%d",&arr[i]);
     }
 
     evennum = 0; // Assuming 0 even numbers
     oddnum  = 0; // Assuming 0 odd numbers
 
-    for(i=0; i<n; i++)
+    for(i=0; i<; i++)       //correct the code
     {
         /* If the current element of array is evennumber then increment evennumber count */
-        if(arr[i]%2 == 0) //missing code
+        if(arr[i]%2 == 0)             
         {
-            ++;
+            ++;              //Correct the code
         }
         else
         {
-            oddnum++; // increment oddnumber count
+            oddnum++; 
         }
 }
-printf("Total number of even numbers and odd numbers in the array are %d and %d",evennum,oddnum  );//missing code
-//printf("Total number of odd numbers in the array : %d\n",   );//missing code
-
+printf("Total number of even numbers and odd numbers in the array are %d and %d",evennum,oddnum);
 return(0);    
 }
