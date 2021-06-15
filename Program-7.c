46c19e0fa68a69fd47fbe470a0a06076ac68d40b
Aditya-CR <54580282+Aditya-CR@users.noreply.github.com>
2021-06-15 15:48:58 +0530

    Update Program-7.c

--- Program-7.c
+++ Program-7.c
@@ -1,18 +1,17 @@
+//Print Array in reverse order
 //INPUT - 3 1 2 3
 //OUTPUT- 3 2 1
-
-//INSERT THE MISSING CODE 
 #include<stdio.h>
 	int main()
 	{
 	  int a[10],i,n;
-	  printf("Enter size of the array and the array elements");
+	  //Read size of the array and the array elements
 	  scanf("%d",&n);
-	  
 	  for(i=0;i<n;i++)
-	  scanf("%d",&a[]);//missing code
-	  printf("Array elements in reverse order : ");
-	  for(i=;i;i) //missing code
+	  scanf("%d",&a[]);                 //Correct the code
+	  
+	  //Display the Array elements in reverse order
+	  for(i=;i;i)                       //Correct the code
 	  printf("%d ",a[i]);
 	  return(0);
 	}
